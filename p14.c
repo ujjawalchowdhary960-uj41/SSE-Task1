05696661cb68ff029033e84bc0c5e1c71194d14f
Ujjawal Chowdhary <ujjawalchowdhary960@gmail.com>
2026-09-26 19:28:26 +0530

    Function first program

---
+++ p14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void very_bad(){
+    printf("good morning\n");
+}
+void good_time(){
+    printf("good afternoon\n");
+}
+void great_time(){
+    printf("good night\n");
+}
+
+int main(){
+    very_bad();
+    good_time();
+    great_time();
+    return 0;
+}
